a0c5a464079caa2e5af560870ef25709ec6c1f81
Lera Abasova <lerman5abas@gmail.com>
2020-10-09 02:48:31 +0300

    first commit

--- chapter4/tryIt.cpp
+++ chapter4/tryIt.cpp
@@ -6,13 +6,16 @@
 int main() {
     std::vector<std::string> words;
     std::string temp;
-    std::string disliked = "new";
+    std::string dislikedFirst = "new";
+    std::string dislikedSecond = "few";
+    std::string dislikedThird = "mew";
+
     while(std::cin>>temp)
         words.push_back(temp);
     std::cout << "The number of words is " << words.size() << std::endl;
 
         for(int i = 0; i < words.size(); ++i) {
-            if(words[i] == disliked)
+            if(words[i] == dislikedFirst || words[i] == dislikedSecond || words[i] == dislikedThird)
                 words[i] = "BLEEP";
             std::cout << words[i] << "\n";
         }
